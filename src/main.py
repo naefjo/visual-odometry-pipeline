467929b2683cb78b503c193ae2906f7a81fa0cd5
Joshua Näf <naefjo@ethz.ch>
2022-12-13 00:18:20 +0100

    estimate bootstrapping pose

--- src/main.py
+++ src/main.py
@@ -7,102 +7,110 @@ from utils import *
 from vo_bootstrap import bootstrap_vo_pipeline
 
 
-dataset = DataSet.PARKING
+def main():
+    dataset = DataSet.PARKING
 
-if dataset == DataSet.KITTI:
-    # TODO: convert matlab code to python code
-    # # need to set kitti_path to folder containing "05" and "poses"
-    # # assert(exist('kitti_path', 'var') ~= 0);
-    # ground_truth = load([kitti_path '/poses/05.txt']);
-    # ground_truth = ground_truth(:, [end-8 end]);
-    # last_frame = 4540;
-    # K = np.array(
-    #     [
-    #         [7.188560000000e+02 0 6.071928000000e+02],
-    #         [0 7.188560000000e+02 1.852157000000e+02],
-    #         [0 0 1],
-    #     ]
-    # )
-    pass
+    if dataset == DataSet.KITTI:
+        # TODO: convert matlab code to python code
+        # # need to set kitti_path to folder containing "05" and "poses"
+        # # assert(exist('kitti_path', 'var') ~= 0);
+        # ground_truth = load([kitti_path '/poses/05.txt']);
+        # ground_truth = ground_truth(:, [end-8 end]);
+        # last_frame = 4540;
+        # K = np.array(
+        #     [
+        #         [7.188560000000e+02 0 6.071928000000e+02],
+        #         [0 7.188560000000e+02 1.852157000000e+02],
+        #         [0 0 1],
+        #     ]
+        # )
+        pass
 
-elif dataset == DataSet.MALAGA:
-    # TODO: convert matlab code to python code
-    # # Path containing the many files of Malaga 7.
-    # assert(exist('malaga_path', 'var') ~= 0);
-    # images = dir([malaga_path ...
-    #     '/malaga-urban-dataset-extract-07_rectified_800x600_Images']);
-    # left_images = images(3:2:end);
-    # last_frame = length(left_images);
-    # K = [621.18428 0 404.0076
-    #     0 621.18428 309.05989
-    #     0 0 1];
-    pass
+    elif dataset == DataSet.MALAGA:
+        # TODO: convert matlab code to python code
+        # # Path containing the many files of Malaga 7.
+        # assert(exist('malaga_path', 'var') ~= 0);
+        # images = dir([malaga_path ...
+        #     '/malaga-urban-dataset-extract-07_rectified_800x600_Images']);
+        # left_images = images(3:2:end);
+        # last_frame = length(left_images);
+        # K = [621.18428 0 404.0076
+        #     0 621.18428 309.05989
+        #     0 0 1];
+        pass
 
-elif dataset == DataSet.PARKING:
-    # Path containing images, depths and all...
-    # assert(exist('parking_path', 'var') ~= 0);
-    last_frame = 598
-    # NOTE: If the following command fails, strip the trailing commas from each line
-    K = np.loadtxt(data_path[dataset] + "K.txt", delimiter=",")
+    elif dataset == DataSet.PARKING:
+        # Path containing images, depths and all...
+        # assert(exist('parking_path', 'var') ~= 0);
+        last_frame = 598
+        bootstrap_frames = [0, 5]
+        # NOTE: If the following command fails, strip the trailing commas from each line
+        K = np.loadtxt(data_path[dataset] + "K.txt", delimiter=",")
 
-    ground_truth = np.loadtxt(data_path[dataset] + "poses.txt")
-    ground_truth = ground_truth[:, -9:]
+        ground_truth = np.loadtxt(data_path[dataset] + "poses.txt")
+        ground_truth = ground_truth[:, -9:]
 
-else:
-    raise NotImplementedError
+    else:
+        raise NotImplementedError
+
+    # Bootstrap
+    # need to set bootstrap_frames
+    if dataset == DataSet.KITTI:
+        # TODO: convert matlab code to python code
+        # img0 = imread([kitti_path '/05/image_0/' ...
+        #     sprintf('%06d.png',bootstrap_frames(1))]);
+        # img1 = imread([kitti_path '/05/image_0/' ...
+        #     sprintf('%06d.png',bootstrap_frames(2))]);
+        pass
+
+    elif dataset == DataSet.MALAGA:
+        # TODO: convert matlab code to python code
+        # img0 = rgb2gray(imread([malaga_path ...
+        #     '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+        #     left_images(bootstrap_frames(1)).name]));
+        # img1 = rgb2gray(imread([malaga_path ...
+        #     '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+        #     left_images(bootstrap_frames(2)).name]));
+        pass
+
+    elif dataset == DataSet.PARKING:
+        img0 = cv2.imread(get_image_path(bootstrap_frames[0], dataset), cv2.IMREAD_GRAYSCALE)
+        img1 = cv2.imread(get_image_path(bootstrap_frames[1], dataset), cv2.IMREAD_GRAYSCALE)
+
+    else:
+        raise NotImplementedError
+
+    # cv2.imshow('', img0)
+    # cv2.waitKey(0)
+    # cv2.imshow('', img1)
+    # cv2.waitKey(0)
+
+    keypoints, estimated_pose, inliner_stats = bootstrap_vo_pipeline(img0, img1, K)
+    
+    
+    # Continuous operation
+    # range = (bootstrap_frames(2)+1):last_frame;
+    for i in range(bootstrap_frames[1],last_frame):
+        pass
+    #     fprintf('\n\nProcessing frame %d\n=====================\n', i);
+    #     if ds == 0
+    #         image = imread([kitti_path '/05/image_0/' sprintf('%06d.png',i)]);
+    #     elseif ds == 1
+    #         image = rgb2gray(imread([malaga_path ...
+    #             '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+    #             left_images(i).name]));
+    #     elseif ds == 2
+    #         image = im2uint8(rgb2gray(imread([parking_path ...
+    #             sprintf('/images/img_%05d.png',i)])));
+    #     else
+    #         assert(false);
+    #     end
+    #     % Makes sure that plots refresh.
+    #     pause(0.01);
+
+    #     prev_img = image;
+    # end
 
 
-# Bootstrap
-# need to set bootstrap_frames
-if dataset == DataSet.KITTI:
-    # TODO: convert matlab code to python code
-    # img0 = imread([kitti_path '/05/image_0/' ...
-    #     sprintf('%06d.png',bootstrap_frames(1))]);
-    # img1 = imread([kitti_path '/05/image_0/' ...
-    #     sprintf('%06d.png',bootstrap_frames(2))]);
-    pass
-
-elif dataset == DataSet.MALAGA:
-    # TODO: convert matlab code to python code
-    # img0 = rgb2gray(imread([malaga_path ...
-    #     '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-    #     left_images(bootstrap_frames(1)).name]));
-    # img1 = rgb2gray(imread([malaga_path ...
-    #     '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-    #     left_images(bootstrap_frames(2)).name]));
-    pass
-
-elif dataset == DataSet.PARKING:
-    img0 = cv2.imread(get_image_path(0, dataset), cv2.IMREAD_GRAYSCALE)
-    img1 = cv2.imread(get_image_path(5, dataset), cv2.IMREAD_GRAYSCALE)
-
-else:
-    raise NotImplementedError
-
-# cv2.imshow('', img0)
-# cv2.waitKey(0)
-# cv2.imshow('', img1)
-# cv2.waitKey(0)
-
-bootstrap_vo_pipeline(img0, img1, K)
-# # Continuous operation
-# range = (bootstrap_frames(2)+1):last_frame;
-# for i = range
-#     fprintf('\n\nProcessing frame %d\n=====================\n', i);
-#     if ds == 0
-#         image = imread([kitti_path '/05/image_0/' sprintf('%06d.png',i)]);
-#     elseif ds == 1
-#         image = rgb2gray(imread([malaga_path ...
-#             '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-#             left_images(i).name]));
-#     elseif ds == 2
-#         image = im2uint8(rgb2gray(imread([parking_path ...
-#             sprintf('/images/img_%05d.png',i)])));
-#     else
-#         assert(false);
-#     end
-#     % Makes sure that plots refresh.
-#     pause(0.01);
-
-#     prev_img = image;
-# end
+if __name__ == "__main__":
+    main()
--- src/vo_bootstrap.py
+++ src/vo_bootstrap.py
@@ -1,29 +1,42 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from typing import Any, Tuple, List
+from numpy.typing import NDArray
 
 from structures import *
 
-from numpy.typing import NDArray
 
-
-def bootstrap_vo_pipeline(img0: cv2.Mat, img1: cv2.Mat, K: NDArray) -> None:
+def bootstrap_vo_pipeline(
+    img0: cv2.Mat,
+    img1: cv2.Mat,
+    K: NDArray,
+) -> Tuple[
+    Tuple[List[cv2.KeyPoint], NDArray], Tuple[NDArray, NDArray], Tuple[int, NDArray]
+]:
     """
     Bootstraps the Visual Odometry pipeline,
 
-    TODO: figure out what inputs/outpus are necessary.
+    Bootstrap the visual odometry pipeline by computing and matching SIFT features
+    between the provided images. Using the resulting machtes, the essential matrix is
+    estimated and the resulting pose estimate is recovered.
 
-    Proposed bootstrapping pipeline:
-    - Run feature extractor on img0 and img1.
-    - Find correspondences between extracted correspondences.
-    - run PnP algorithm to estimate 3D landmark position of found features.
-    - Retrun found landmarks.
+    based on: https://docs.opencv.org/4.x/da/de9/tutorial_py_epipolar_geometry.html
+
+    Args:
+        img0: The first image used for bootstrapping the VO pipeline.
+        img1: The second image used for bootstrapping the VO pipeline.
+        K: Intrinsic camera matrik (3x3)
+
+    Returns:
+        keypoints: Tuple containing keypoints in img1 and their corresponding descriptor
+        estimated_pose: Tuple of rotation matrix and scaled translation vector which expresses
+          the coordinate system of camera 0 in the coordinate system of camera 1.
+        inliner_stats: Tuple of number of inliers and inlier mask.
+
     """
 
     sift = cv2.SIFT_create()
-
-    # create a brute force matcher
-    bf_matcher = cv2.BFMatcher_create()
 
     keypoints_img0, keypoint_descriptor_img0 = sift.detectAndCompute(img0, None)
     keypoints_img1, keypoint_descriptor_img1 = sift.detectAndCompute(img1, None)
@@ -41,10 +54,9 @@ def bootstrap_vo_pipeline(img0: cv2.Mat, img1: cv2.Mat, K: NDArray) -> None:
     # cv2.waitKey(0)
 
     # Match SIFT feature descriptors and sort them by decreasing distance.
-    matched_keypoints = bf_matcher.match(
-        keypoint_descriptor_img0, keypoint_descriptor_img1
-    )
-    matched_keypoints = sorted(matched_keypoints, key=lambda x: x.distance)
+    # matched_keypoints = bf_matcher.match(
+    #     keypoint_descriptor_img0, keypoint_descriptor_img1
+    # )
 
     # Draw matches.
     # img3 = cv2.drawMatches(
@@ -58,3 +70,50 @@ def bootstrap_vo_pipeline(img0: cv2.Mat, img1: cv2.Mat, K: NDArray) -> None:
     # )
     # plt.imshow(img3)
     # plt.show(block=True)
+
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann_matcher = cv2.FlannBasedMatcher(index_params, search_params)
+    matched_keypoints = flann_matcher.knnMatch(
+        keypoint_descriptor_img0, keypoint_descriptor_img1, k=2
+    )
+
+    # Sort matched keypoints according to keypoint distance
+    # matched_keypoints = sorted(matched_keypoints, key=lambda x: x.distance)
+
+    points_img0 = []
+    points_img1 = []
+
+    for (m, n) in matched_keypoints:
+        if m.distance < 0.8 * n.distance:
+            points_img0.append(keypoints_img0[m.queryIdx].pt)
+            points_img1.append(keypoints_img1[m.trainIdx].pt)
+
+    points_img0 = np.int32(points_img0)
+    points_img1 = np.int32(points_img1)
+
+    essential_matrix, inlier_mask = cv2.findEssentialMat(
+        points_img0,
+        points_img1,
+        K,
+        method=cv2.RANSAC,
+        prob=0.99,
+    )
+
+    # [recovered_rotation, recovered_position] = T_{1,0}, i.e. we convert vectors from the first
+    # camera frame into vectors in the second camera frame.
+    # NOTE(@naefjo): translation is up to scale/unit length
+    num_inliers, recovered_rotation, recovered_position, inlier_mask = cv2.recoverPose(
+        essential_matrix,
+        points_img0,
+        points_img1,
+        K,
+        inlier_mask,
+    )
+
+    return (
+        (keypoints_img1, keypoint_descriptor_img1),
+        (recovered_rotation, recovered_position),
+        (num_inliers, inlier_mask),
+    )
